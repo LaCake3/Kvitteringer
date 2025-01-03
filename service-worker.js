f9b8c51659ec008494f2b2550a0eb8cb2ba0472d
LaCake3 <daniel.lindhardt@me.com>
2025-01-03 13:31:47 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const cacheName = "pdf-generator-v1";
+const staticAssets = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json",
+    "./icon-192x192.png",
+    "./icon-512x512.png"
+];
+
+self.addEventListener("install", async event => {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(staticAssets);
+});
+
+self.addEventListener("fetch", event => {
+    const request = event.request;
+    event.respondWith(cacheFirst(request));
+});
+
+async function cacheFirst(request) {
+    const cachedResponse = await caches.match(request);
+    return cachedResponse || fetch(request);
+}

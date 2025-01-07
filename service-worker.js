e85316459cf77dfed5cc2f1d483752163c846b4e
LaCake3 <daniel.lindhardt@me.com>
2025-01-07 08:15:49 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = "pdf-generator-v11"; // Skift cache-navn ved nye versioner
+const cacheName = "pdf-generator-v11.1"; // Skift cache-navn ved nye versioner
 const staticAssets = [
     "./",
     "./index.html",
